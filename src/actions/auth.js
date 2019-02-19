8403bcd1185dbc60b100e99a337335a70ad93a5e
GeoHernandez27 <leopardo2@gmail.com>
2019-02-18 18:15:26 -0700

    handled signup post request

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -1,13 +1,14 @@
-import axio from 'axios';
+import axios from 'axios';
 
 import { ROOT_URL } from '../config';
 
-export function singUp(fields, success) {
-    console.log(fields);
+export function signUp(fields, success) {
+    console.log(fields);  
     return function(dispatch) {
         axios.post (`${ROOT_URL}/signUp`, fields)
             .then(response => {
-                //do something
+                console.log(response);
+                success()
             })
             .catch(err => {
                 if(err) { console.log(err)}
