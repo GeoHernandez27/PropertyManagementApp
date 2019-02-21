01484b3df93072f1adaefc2c02fe3685cce69fc1
GeoHernandez27 <leopardo2@gmail.com>
2019-02-20 20:33:40 -0700

    built newsletter latest component and grid, refactored newslettersgrid.

--- src/components/newsletter/newsletterGrid.js
+++ src/components/newsletter/newsletterGrid.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 
 import NewsletterBox from './newsletterBox';
 import NewsletterArchive from './newsletterArchive';
+import NewsletterLatest from './newsletterLatest';
 
 
 class NewsletterGrid extends Component {
@@ -11,7 +12,7 @@ class NewsletterGrid extends Component {
                 {/*add button*/}
                 <NewsletterBox date={new Date()}/>
                 <NewsletterArchive/>
-                {/*content*/}
+                <NewsletterLatest/>
             </div>
         )
     }
