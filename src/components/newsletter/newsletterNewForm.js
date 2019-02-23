54a9c6f62585dadb150790d746a5e0575c8f8ca6
GeoHernandez27 <leopardo2@gmail.com>
2019-02-23 11:19:30 -0700

    customized newsletter new form

--- src/components/newsletter/newsletterNewForm.js
+++ src/components/newsletter/newsletterNewForm.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import { reduxForm, Field } from "redux-form";
 
 import { FormTitle } from "../formTitle";
-import { FormInput, FormButton } from "../formFields";
+import { FormInput, FormButton, FormTextArea } from "../formFields";
 import Textlink from "../textLink";
 
 class NewNewsletterForm
@@ -13,16 +13,24 @@ class NewNewsletterForm
 
     return (
       <form onSubmit={handleSubmit} className="new-newsletter-form">
-        <FormTitle className="new-newsletter-form__title" text="Login" />
+        <FormTitle className="new-newsletter-form__title" text="New Newsletter" />
         <Field
-          className="new-newsletter-form__email"
-          placeholder="Enter Email"
-          name="email"
-          type="email"
-          title='Email'
+          className="new-newsletter-form__newsletter-title"
+          placeholder="Newsletter Title"
+          name="title"
+          type="text"
+          title="Newsletter Title"
           component={FormInput}
         />
-
+         <Field
+          className="new-newsletter-form__body"
+          placeholder="Newsletter Body"
+          name="body"
+          type="text"
+          title='Body'
+          component={FormTextArea}
+        />
+        
       </form>
     );
   }
