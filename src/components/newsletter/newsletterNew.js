fff3625a0f2606c2ec11ef5d721df6785993185c
GeoHernandez27 <leopardo2@gmail.com>
2019-02-23 12:19:27 -0700

    built cancel button and placed form buttons on grid

--- src/components/newsletter/newsletterNew.js
+++ src/components/newsletter/newsletterNew.js
@@ -6,14 +6,18 @@ import NewNewsletterForm from './newsletterNewForm';
 
 class NewNewsletter extends Component {
     
-    onSubmit = (fields) => {
-        console.log('trying to handle submit')
+    onSubmit = (fields, button) => {
+        console.log('trying to submit');
     }
+    onCancel = () => {
+        console.log('trying to cancel');
+    }
+        
 
     render() {
         return(
             <div className='new-newsletter'>
-                <NewNewsletterForm onSubmit={(event) => this.onSubmit(event)}/>
+                <NewNewsletterForm onCancel={() => this.onCancel()} onSubmit={(event) => this.onSubmit(event)}/>
             </div>
         )
     }
--- src/components/newsletter/newsletterNewForm.js
+++ src/components/newsletter/newsletterNewForm.js
@@ -31,7 +31,16 @@ class NewNewsletterForm
           component={FormTextArea}
         />
         <Field
-          className="sign-in-form__submit"
+          className="new-newsletter-form__cancel"
+          small={true}
+          name="cancel"
+          type="button"
+          title="Cancel"
+          component={FormButton}
+          onClick={this.props.onCancel}
+        />
+        <Field
+          className="new-newsletter-form__submit"
           small={true}
           danger={true}
           name="submit"
