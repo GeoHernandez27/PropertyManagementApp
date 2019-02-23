dee3917e8cec89c37584ee41fd238fe7baaf8138
GeoHernandez27 <leopardo2@gmail.com>
2019-02-23 11:42:41 -0700

    added additional button option to form-button component

--- src/components/newsletter/newsletterNewForm.js
+++ src/components/newsletter/newsletterNewForm.js
@@ -30,6 +30,15 @@ class NewNewsletterForm
           title='Body'
           component={FormTextArea}
         />
+        <Field
+          className="sign-in-form__submit"
+          small={true}
+          danger={true}
+          name="submit"
+          type="submit"
+          title="Submit"
+          component={FormButton}
+        />
         
       </form>
     );
