d999bfc5e1d292a8d0f12e7a5a67bf7565a5668a
GeoHernandez27 <leopardo2@gmail.com>
2019-02-25 20:56:46 -0700

    hooked up latestnewsletter and datebox component to store to fetch data

--- src/components/newsletter/newsletterBox.js
+++ src/components/newsletter/newsletterBox.js
@@ -1,11 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
-export default function NewsletterBox({date}) {
-    return (
+
+
+class  NewsletterBox extends Component {
+    render() {
+        const { date} = this.props;
+        if(!date) {
+            return <div>...Fetching Data</div>
+        }
+        return (
         <div className='newsletter-box'>
             <div className='newsletter-box__day'>{date.getDate()}</div>
             <div className='newsletter-box__month-year'>{date.toLocaleString('en-us', {month: 'short'})} {date.getFullYear()}</div>
             <div className='newsletter-box__point'></div>
         </div>
     )
+    }
 }
+
+function mapStateToProps(state) {
+    const { newsletters } = state.newsletters;
+    const latestNewsletter = newsletters[0];
+    return {
+        ...latestNewsletter
+    }
+}
+
+export default connect(mapStateToProps)(NewsletterBox);
--- src/components/newsletter/newsletterGrid.js
+++ src/components/newsletter/newsletterGrid.js
@@ -15,26 +15,19 @@ class NewsletterGrid extends Component {
     }
 
     componentDidMount() {
-        this.props.fetchNewsletters();
+        setTimeout(() => {
+            this.props.fetchNewsletters();
+        }, 1000);
+        
     }
 
     render() {
-        
-        const latest = {
-            _id: '115',
-            title: 'Happy Holidays Fam',
-            body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ullamcorper eget nulla facilisi etiam dignissim. Diam maecenas ultricies mi eget mauris pharetra et. Netus et malesuada fames ac turpis egestas. Id porta nibh venenatis cras sed. Etiam tempor orci eu lobortis elementum nibh tellus molestie nunc. Risus pretium quam vulputate dignissim suspendisse in. Eget aliquet nibh praesent tristique magna sit amet purus. Ac felis donec et odio. Mattis vulputate enim nulla aliquet porttitor lacus luctus accumsan. Elit scelerisque mauris pellentesque pulvinar pellentesque habitant morbi tristique senectus. Integer feugiat scelerisque varius morbi enim nunc faucibus. Sapien et ligula ullamcorper malesuada. Bibendum arcu vitae elementum curabitur vitae. Purus gravida quis blandit turpis cursus in hac. Facilisis magna etiam tempor orci eu lobortis. Ultrices dui sapien eget mi proin sed. Erat imperdiet sed euismod nisi porta. Lacus sed viverra tellus in hac habitasse platea dictumst vestibulum. Ornare arcu dui vivamus arcu felis. Lacus suspendisse faucibus interdum posuere lorem ipsum dolor sit. Aenean euismod elementum nisi quis eleifend quam adipiscing.', 
-            date: new Date(),
-            imageUrl: 'http://via.placeholder.com/960x258'
-
-        }
-
-        return (
+         return (
             <div className='newsletter-grid'>
                 <Button className='newsletter-grid__button' icon='fas fa-plus' callback={() => this.handleAddNewsletter()} />
-                <NewsletterBox date={new Date()}/>
+                <NewsletterBox/>
                 <NewsletterArchive/>
-                <NewsletterLatest history={this.props.history} {...latest}/>
+                <NewsletterLatest history={this.props.history} />
             </div>
         )
     }
